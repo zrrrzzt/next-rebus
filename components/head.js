0b0b14eefc7b81c24d0cca5637a640747d0fb958
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-12-25 09:43:03 +0100

    standard --fix (patch)

--- components/head.js
+++ components/head.js
@@ -1,13 +1,11 @@
-'use strict'
-
 import Head from 'next/head'
 
 export default () => (
   <div>
     <Head>
-      <title>bigfive compare</title>
+      <title>Juleskatten</title>
       <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      <link href='//cdn.muicss.com/mui-0.9.15/css/mui.min.css' rel='stylesheet' type='text/css' media='screen' />
+      <link href='//cdn.muicss.com/mui-0.9.41/css/mui.min.css' rel='stylesheet' type='text/css' media='screen' />
       <link href='static/css/app.css' rel='stylesheet' type='text/css' media='screen' />
       <link rel='apple-touch-icon' sizes='152x152' href='static/images/icons/apple-touch-icon.png' />
       <link rel='icon' type='image/png' href='static/images/icons/favicon-32x32.png' sizes='32x32' />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 import React from 'react'
 import { Notification } from 'react-notification'
 import Container from 'muicss/lib/react/container'
